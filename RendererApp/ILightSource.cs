3b04672ca8bcf2f52d1f554daf70df613c11dcd2
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-05-10 15:40:08 +0300

    fix: fix transform matrix

---
+++ RendererApp/ILightSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RendererApp
+{
+    interface ILightSource
+    {
+        public float Intencity { get; set; }
+    }
+}
---
+++ RendererApp/RendererApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RendererApp
+{
+    public class RendererApp
+    {
+
+        private string sourcePath;
+        private string outputPath;
+
+
+        public void Start(string[] args)
+        {
+            ParseInputLine(args);
+            Renderer renderer = new Renderer(width: 800, angelX: 4.71238898048);
+            renderer.Render(sourcePath, outputPath);
+        }
+
+        private void ParseInputLine(string[] args)
+        {
+            if (args.Length == 0)
+                throw new Exception("Right format: Renderer.exe --source=[file-path] --output=[output-file-path]");
+            if (!IsCorrectAttrs(args))
+                throw new Exception("Incorrect response. Right format: --source=[file-path] --output=[output-file-path]");
+            GetSourcePath(args[0]);
+            GetGoalPath(args[1]);
+        }
+
+        private void GetGoalPath(string output)
+        {
+            outputPath = output.Substring(9);
+        }
+
+        private void GetSourcePath(string source)
+        {
+            sourcePath = source.Substring(9);
+        }
+
+        private bool IsCorrectAttrs(string[] args)
+        {
+            var sourceRegex = new Regex(@"--source=(\S+)\.(\w+)");
+            Regex goalRegex = new Regex(@"--output=(\S+)\.(\w+)");
+
+            if (sourceRegex.IsMatch(args[0]) && goalRegex.IsMatch(args[1]))
+                return true;
+
+            return false;
+        }
+    }
+}
